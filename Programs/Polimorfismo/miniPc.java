401ed46f77088dcf6444ee2acaa87afb318970ee
Rafael Kozlowski Andreola <rafandreola@hotmail.com>
2022-09-23 09:47:06 -0300

    Add files via upload

---
+++ Programs/Polimorfismo/miniPc.java
@@ -0,0 +1,10 @@
+public class miniPc extends produto {
+
+    public double peso = 500;
+    public double volume = 200;
+    public double preco = 5000;
+
+    public double calculaFrete() {
+        return super.calculaFrete();
+    }
+}
---
+++ Programs/Polimorfismo/soundBar.java
@@ -0,0 +1,10 @@
+public class soundBar extends produto {
+
+    public double peso = 670;
+    public double volume = 8000;
+    public double preco = 1800;
+
+    public double calculaFrete() {
+        return super.calculaFrete();
+    }
+}
